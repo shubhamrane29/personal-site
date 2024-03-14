fc1f900c38e185237ad69cb7a0463b43d290bafb
shubhamrane29 <shubhrane2911@gmail.com>
2024-03-14 16:51:45 +0530

    Flask SQL Integration

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql6690817:83KC351EUK@sql6.freesqldatabase.com:3306/sql6690817'
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQL_KEY")
 db = SQLAlchemy(app)
 
 class Subs(db.Model):
     sno= db.Column(db.Integer, primary_key = True)
     name= db.Column(db.String(200), nullable=False)
     email= db.Column(db.String(200), nullable=False)
-    date = db.Column(db.DateTime, default=lambda: datetime.utcnow().replace(microsecond=0))
+    date = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc).replace(microsecond=0))
 
     def __repr__(self):
         return f"{self.sno} - {self.title}"
